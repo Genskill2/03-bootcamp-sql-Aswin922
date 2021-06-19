71101e32d975d69f76903859d39721e723f8de6f
Aswin922 <aswin_b180922me@nitc.ac.in>
2021-06-19 21:33:02 +0530

    All files

--- query3.sql
+++
@@ -1,1 +0,0 @@
-select b.title from books b,subjects s,books_subjects j where j.book = b.id and j.subject = s.id and s.name in ('Technology','Politics');
--- query4.sql
+++
@@ -1,1 +0,0 @@
-select s.name from books b,subjects s,books_subjects j where j.book = b.id and j.subject = s.id and b.title = "Atomic Habits";
